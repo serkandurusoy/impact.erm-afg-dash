f73e97b75403a3dd5d4ea34048f599fd768abc74
Piotr Smykaj <piotr.smykaj@gmail.com>
2018-04-12 11:50:38 +0200

    dist removed

--- prototypes/dist/js/impact.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-  $('.navbar-toggler').on('click', function(e) {
-    $('.navbar-collapse').toggleClass('show');
-  });
-
-  $('.nav-search__link').on('click', function() {
-    $(this).parents('.nav-search').toggleClass('active');
-  });
-})(jQuery);
