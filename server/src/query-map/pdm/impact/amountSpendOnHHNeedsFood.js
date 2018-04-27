f1ac627b5eba12b4ecc1a13764aefdd8f342aaa8
serkandurusoy <serkan.durusoy@dna-tr.com>
2018-04-28 00:40:35 +0300

    query clean up

--- server/src/query-map/pdm/impact/amountSpendOnHHNeedsFood.js
+++ server/src/query-map/pdm/impact/amountSpendOnHHNeedsFood.js
@@ -3,120 +3,65 @@ export default async (
   // eslint-disable-next-line no-unused-vars
   { provinces, districts, dateBegin, dateEnd },
 ) => {
-  const [results] = await database.raw(`SELECT
-    IF(COUNT(\`s1_household_profile/head_of_household/female_hhd\`) > 0,
-        's1_household_profile/head_of_household/female_hhd',
-        NULL) AS \`Head of Household\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_nothing',
-        1,
-        0)) AS \`almost_nothing\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'less_than_half',
-        1,
-        0)) AS \`less_than_half\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'more_than_half',
-        1,
-        0)) AS \`more_than_half\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_all',
-        1,
-        0)) AS \`almost_all\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = '',
-        1,
-        0)) AS \`N/A\`
-FROM
-    pdm
-WHERE
-    \`s1_household_profile/head_of_household/female_hhd\` = 'TRUE'
-UNION SELECT
-    IF(COUNT(\`s1_household_profile/head_of_household/child_hhd\`) > 0,
-        's1_household_profile/head_of_household/child_hhd',
-        NULL) AS \`Head of Household \`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_nothing',
-        1,
-        0)) AS \`almost_nothing\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'less_than_half',
-        1,
-        0)) AS \`less_than_half\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'more_than_half',
-        1,
-        0)) AS \`more_than_half\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_all',
-        1,
-        0)) AS \`almost_all\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = '',
-        1,
-        0)) AS \`N/A\`
-FROM
-    pdm
-WHERE
-    \`s1_household_profile/head_of_household/child_hhd\` = 'TRUE'
-UNION SELECT
-    IF(COUNT(\`s1_household_profile/head_of_household/elderly_hhd\`) > 0,
-        's1_household_profile/head_of_household/elderly_hhd',
-        NULL) AS \`Head of Household for Food\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_nothing',
-        1,
-        0)) AS \`almost_nothing\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'less_than_half',
-        1,
-        0)) AS \`less_than_half\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'more_than_half',
-        1,
-        0)) AS \`more_than_half\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_all',
-        1,
-        0)) AS \`almost_all\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = '',
-        1,
-        0)) AS \`N/A\`
-FROM
-    pdm
-WHERE
-    \`s1_household_profile/head_of_household/elderly_hhd\` = 'TRUE'
-UNION SELECT
-    IF(COUNT(\`s1_household_profile/head_of_household/chronically_ill\`) > 0,
-        's1_household_profile/head_of_household/chronically_ill',
-        NULL) AS \`Head of Household  for Food\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_nothing',
-        1,
-        0)) AS \`almost_nothing\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'less_than_half',
-        1,
-        0)) AS \`less_than_half\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'more_than_half',
-        1,
-        0)) AS \`more_than_half\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_all',
-        1,
-        0)) AS \`almost_all\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = '',
-        1,
-        0)) AS \`N/A\`
-FROM
-    pdm
-WHERE
-    \`s1_household_profile/head_of_household/chronically_ill\` = 'TRUE'
-UNION SELECT
-    IF(COUNT(\`s1_household_profile/head_of_household/hh_disability\`) > 0,
-        's1_household_profile/head_of_household/hh_disability',
-        NULL) AS \`Head of Household  for Food\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_nothing',
-        1,
-        0)) AS \`almost_nothing\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'less_than_half',
-        1,
-        0)) AS \`less_than_half\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'more_than_half',
-        1,
-        0)) AS \`more_than_half\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_all',
-        1,
-        0)) AS \`almost_all\`,
-    SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = '',
-        1,
-        0)) AS \`N/A\`
-FROM
-    pdm
-WHERE
-    \`s1_household_profile/head_of_household/hh_disability\` = 'TRUE';`);
+  const [results] = await database.raw(
+    `
+    SELECT
+        IF(COUNT(\`s1_household_profile/head_of_household/female_hhd\`) > 0, 's1_household_profile/head_of_household/female_hhd', NULL) AS \`Head of Household\`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_nothing', 1, 0)) AS \`almost_nothing' \`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'less_than_half', 1, 0)) AS \`less_than_half'\`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'more_than_half', 1, 0)) AS \`more_than_half\`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_all', 1, 0)) AS \`almost_all'\`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = '', 1, 0)) AS \`N/A\`
+    FROM
+        pdm
+    WHERE
+        \`s1_household_profile/head_of_household/female_hhd\` = 'TRUE'
+    UNION SELECT
+        IF(COUNT(\`s1_household_profile/head_of_household/child_hhd\`) > 0, 's1_household_profile/head_of_household/child_hhd', NULL) AS \`Head of Household \`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_nothing', 1, 0)) AS \`almost_nothing' \`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'less_than_half', 1, 0)) AS \`less_than_half'\`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'more_than_half', 1, 0)) AS \`more_than_half\`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_all', 1, 0)) AS \`almost_all'\`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = '', 1, 0)) AS \`N/A\`
+    FROM
+        pdm
+    WHERE
+        \`s1_household_profile/head_of_household/child_hhd\` = 'TRUE'
+    UNION SELECT
+        IF(COUNT(\`s1_household_profile/head_of_household/elderly_hhd\`) > 0, 's1_household_profile/head_of_household/elderly_hhd', NULL) AS \`Head of Household for Blankets \`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_nothing', 1, 0)) AS \`almost_nothing' \`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'less_than_half', 1, 0)) AS \`less_than_half'\`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'more_than_half', 1, 0)) AS \`more_than_half\`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_all', 1, 0)) AS \`almost_all'\`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = '', 1, 0)) AS \`N/A\`
+    FROM
+        pdm
+    WHERE
+        \`s1_household_profile/head_of_household/elderly_hhd\` = 'TRUE'
+    UNION SELECT
+        IF(COUNT(\`s1_household_profile/head_of_household/chronically_ill\`) > 0, 's1_household_profile/head_of_household/chronically_ill', NULL) AS \`Head of Household  for Blankets \`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_nothing', 1, 0)) AS \`almost_nothing' \`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'less_than_half', 1, 0)) AS \`less_than_half'\`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'more_than_half', 1, 0)) AS \`more_than_half\`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_all', 1, 0)) AS \`almost_all'\`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = '', 1, 0)) AS \`N/A\`
+    FROM
+        pdm
+    WHERE
+        \`s1_household_profile/head_of_household/chronically_ill\` = 'TRUE'
+    UNION SELECT
+        IF(COUNT(\`s1_household_profile/head_of_household/hh_disability\`) > 0, 's1_household_profile/head_of_household/hh_disability', NULL) AS \`Head of Household  for Blankets \`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_nothing', 1, 0)) AS \`almost_nothing' \`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'less_than_half', 1, 0)) AS \`less_than_half'\`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'more_than_half', 1, 0)) AS \`more_than_half\`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = 'almost_all', 1, 0)) AS \`almost_all'\`,
+        SUM(IF(\`s3_use_of_cash_assistanceq3_5_1_food\` = '', 1, 0)) AS \`N/A\`
+    FROM
+        pdm
+    WHERE
+        \`s1_household_profile/head_of_household/hh_disability\` = 'TRUE'
+    ;`,
+  );
+
   return results;
 };
