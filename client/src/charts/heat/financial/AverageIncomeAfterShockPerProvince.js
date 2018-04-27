850e3cce5e02efe3f1be9b3b2ed549213670babc
ric0 <e.cerroni@gmail.com>
2018-04-27 17:17:08 +0200

    PDM | Average Income After Shock per Province | ChoroplethMap

--- client/src/charts/heat/financial/AverageIncomeAfterShockPerProvince.js
+++ client/src/charts/heat/financial/AverageIncomeAfterShockPerProvince.js
@@ -1,20 +1,77 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  ComposableMap,
+  ZoomableGroup,
+  Geographies,
+  Geography,
+} from 'react-simple-maps';
+import chroma from 'chroma-js';
+import PROVINCE_INFO from '../../../constants/province-info';
+import PROVINCE_GEO_DATA from '../../../constants/province-geo-data';
+
+const colorScale = chroma.scale(['ffffff', 'ee4e4e']);
 
 const Chart = ({ data }) => (
   <div
     style={{
-      backgroundColor: '#696969',
-      color: '#ffffff',
-      textAlign: 'left',
-      lineHeight: 1.5,
       width: '100%',
-      height: '100%',
-      overflow: 'auto',
-      fontSize: 10,
+      maxWidth: 980,
+      margin: '0 auto',
     }}
   >
-    <pre>{JSON.stringify(data, null, 2)}</pre>
+    <ComposableMap width={600} height={500}>
+      <ZoomableGroup zoom={18} center={[67, 34]} disablePanning>
+        <Geographies geography={PROVINCE_GEO_DATA}>
+          {(geographies, projection) => {
+            const maxAvg = Math.max(
+              ...data.map(d => d.avg_S4_financial_ASSESSq4_4_income_after),
+            );
+            return geographies.map(geography => {
+              const provinceName = PROVINCE_INFO.find(
+                ({ id }) => id === geography.properties.ID_1,
+              ).name;
+              const province = data.find(
+                d => d.general_infoq1_province === provinceName,
+              );
+              const color =
+                province &&
+                province.avg_S4_financial_ASSESSq4_4_income_after &&
+                colorScale(
+                  province.avg_S4_financial_ASSESSq4_4_income_after / maxAvg,
+                ).hex();
+              return (
+                <Geography
+                  key={geography.properties.ID_1}
+                  geography={geography}
+                  projection={projection}
+                  style={{
+                    default: {
+                      fill: color || '#ffffff',
+                      stroke: '#607D8B',
+                      strokeWidth: 0.01,
+                      outline: 'none',
+                    },
+                    hover: {
+                      fill: '#607D8B',
+                      stroke: '#607D8B',
+                      strokeWidth: 0.01,
+                      outline: 'none',
+                    },
+                    pressed: {
+                      fill: '#607D8B',
+                      stroke: '#607D8B',
+                      strokeWidth: 0.01,
+                      outline: 'none',
+                    },
+                  }}
+                />
+              );
+            });
+          }}
+        </Geographies>
+      </ZoomableGroup>
+    </ComposableMap>
   </div>
 );
 
