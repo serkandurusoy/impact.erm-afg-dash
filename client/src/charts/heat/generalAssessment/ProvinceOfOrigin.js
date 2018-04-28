96e2bb0637db1e80119307f8944441b120ae753c
ric0 <e.cerroni@gmail.com>
2018-04-28 14:40:53 +0200

    HEAT | Reported Province of Origin | Treemap -> BarChart

--- client/src/charts/heat/generalAssessment/ProvinceOfOrigin.js
+++ client/src/charts/heat/generalAssessment/ProvinceOfOrigin.js
@@ -1,81 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Treemap } from 'recharts';
-
-const COLORS = [
-  '#8889DD',
-  '#9597E4',
-  '#8DC77B',
-  '#A5D297',
-  '#E2CF45',
-  '#F8C12D',
-];
-/* eslint-disable */
-const CustomizedContent = ({
-  root,
-  depth,
-  x,
-  y,
-  width,
-  height,
-  index,
-  colors,
-  name,
-}) => (
-  <g>
-    <rect
-      x={x}
-      y={y}
-      width={width}
-      height={height}
-      style={{
-        fill:
-          depth < 2
-            ? colors[Math.floor(index / root.children.length * 6)]
-            : 'none',
-        stroke: '#fff',
-        strokeWidth: 2 / (depth + 1e-10),
-        strokeOpacity: 1 / (depth + 1e-10),
-      }}
-    />
-    {depth === 1 ? (
-      <text
-        x={x + width / 2}
-        y={y + height / 2 + 7}
-        textAnchor="middle"
-        fill="#fff"
-        fontSize={14}
-      >
-        {name}
-      </text>
-    ) : null}
-    {depth === 1 ? (
-      <text x={x + 4} y={y + 18} fill="#fff" fontSize={16} fillOpacity={0.9}>
-        {index + 1}
-      </text>
-    ) : null}
-  </g>
-);
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
 
 const Chart = ({ data }) => (
-  <Treemap
-    width={800}
-    height={400}
-    data={data.map(d => { console.log(d.count); return {
-      name: d.S3_GEN_ASSESSq3_2_1_province_origin,
-      children: [
-        {
-          name: d.S3_GEN_ASSESSq3_2_1_province_origin,
-          value: d.count,
-        },
-      ],
-    }})}
-    dataKey="value"
-    ratio={4 / 3}
-    stroke="#fff"
-    fill="#8884d8"
-    content={<CustomizedContent colors={COLORS} />}
-  />
+  <ResponsiveContainer height={400}>
+    <BarChart
+      data={data.map(d => ({ ...d, x: d.S3_GEN_ASSESSq3_2_1_province_origin }))}
+    >
+      <XAxis
+        dataKey="x"
+        height={80}
+        angle={90}
+        interval={0}
+        tickMargin={40}
+        tickCount={1}
+      />
+      <YAxis />
+      <Tooltip
+        content={({ payload, label }) =>
+          payload[0] && (
+            <div className="graph__tooltip">
+              {payload[0].payload.count} people are from {label}
+            </div>
+          )
+        }
+      />
+      <Bar dataKey="count" fill="#ee4e4e" />
+    </BarChart>
+  </ResponsiveContainer>
 );
 
 Chart.propTypes = {
