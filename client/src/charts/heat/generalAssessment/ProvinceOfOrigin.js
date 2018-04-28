b4c5547f5bfea03d68580391bf31b0cd27ab1a03
ric0 <e.cerroni@gmail.com>
2018-04-28 14:24:19 +0200

    Add legend to all PieCharts committed in the previous days

--- client/src/charts/heat/generalAssessment/ProvinceOfOrigin.js
+++ client/src/charts/heat/generalAssessment/ProvinceOfOrigin.js
@@ -2,11 +2,66 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Treemap } from 'recharts';
 
+const COLORS = [
+  '#8889DD',
+  '#9597E4',
+  '#8DC77B',
+  '#A5D297',
+  '#E2CF45',
+  '#F8C12D',
+];
+/* eslint-disable */
+const CustomizedContent = ({
+  root,
+  depth,
+  x,
+  y,
+  width,
+  height,
+  index,
+  colors,
+  name,
+}) => (
+  <g>
+    <rect
+      x={x}
+      y={y}
+      width={width}
+      height={height}
+      style={{
+        fill:
+          depth < 2
+            ? colors[Math.floor(index / root.children.length * 6)]
+            : 'none',
+        stroke: '#fff',
+        strokeWidth: 2 / (depth + 1e-10),
+        strokeOpacity: 1 / (depth + 1e-10),
+      }}
+    />
+    {depth === 1 ? (
+      <text
+        x={x + width / 2}
+        y={y + height / 2 + 7}
+        textAnchor="middle"
+        fill="#fff"
+        fontSize={14}
+      >
+        {name}
+      </text>
+    ) : null}
+    {depth === 1 ? (
+      <text x={x + 4} y={y + 18} fill="#fff" fontSize={16} fillOpacity={0.9}>
+        {index + 1}
+      </text>
+    ) : null}
+  </g>
+);
+
 const Chart = ({ data }) => (
   <Treemap
     width={800}
     height={400}
-    data={data.map(d => ({
+    data={data.map(d => { console.log(d.count); return {
       name: d.S3_GEN_ASSESSq3_2_1_province_origin,
       children: [
         {
@@ -14,11 +69,12 @@ const Chart = ({ data }) => (
           value: d.count,
         },
       ],
-    }))}
+    }})}
     dataKey="value"
     ratio={4 / 3}
     stroke="#fff"
     fill="#8884d8"
+    content={<CustomizedContent colors={COLORS} />}
   />
 );
 
--- client/src/charts/pdm/impact/LevelOfDebtCompared.js
+++ client/src/charts/pdm/impact/LevelOfDebtCompared.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { PieChart, Pie, Tooltip, Cell } from 'recharts';
 import chroma from 'chroma-js';
 
-const colorScale = chroma.scale(['ffffff', 'ee4e4e']);
+const colorScale = chroma.scale(['c0c0c0', 'ee4e4e']);
 
 const Chart = ({ data }) => {
   const dataMap = Object.entries(data[0]).map(([k, v]) => ({
@@ -21,6 +21,7 @@ const Chart = ({ data }) => {
         cy="50%"
         outerRadius={150}
         fill="#8884d8"
+        label
       >
         {dataMap.map(d => (
           <Cell
