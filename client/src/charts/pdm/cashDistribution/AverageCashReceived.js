86360c81492beac2b1d146f4e0336465054955cc
ric0 <e.cerroni@gmail.com>
2018-04-29 21:47:51 +0200

    PDM | Reported Average Amount of Cash Assistance Received | BulletChart

--- client/src/charts/pdm/cashDistribution/AverageCashReceived.js
+++ client/src/charts/pdm/cashDistribution/AverageCashReceived.js
@@ -1,25 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import BulletGraph from 'react-bullet-graph-react16';
+import ReactTooltip from 'react-tooltip';
 
-const Chart = ({ data }) => (
-  <div
-    style={{
-      backgroundColor: '#696969',
-      color: '#ffffff',
-      textAlign: 'left',
-      lineHeight: 1.5,
-      width: '100%',
-      height: '100%',
-      overflow: 'auto',
-      fontSize: 10,
-    }}
-  >
-    <pre>{JSON.stringify(data, null, 2)}</pre>
-  </div>
-);
+class Chart extends Component {
+  static propTypes = {
+    data: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
+  };
 
-Chart.propTypes = {
-  data: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
-};
+  componentDidMount() {
+    setTimeout(() => {
+      ReactTooltip.rebuild();
+    }, 100);
+  }
+
+  render() {
+    const { data } = this.props;
+    return (
+      <div
+        style={{
+          width: '100%',
+          maxWidth: 980,
+          margin: '0 auto',
+        }}
+      >
+        <div
+          style={{ width: 600, height: 40, margin: '0 auto' }}
+          data-for="averageCashReceived"
+          data-tip={`
+            <div style="padding: 10px; line-height: 1.5; text-align: left">
+              Min: ${data.min_s2_cash_distribution_processq2_3_how_mach_cash}
+              <br />
+              Max: ${data.max_s2_cash_distribution_processq2_3_how_mach_cash}
+              <br />
+              Avg: ${data.avg_s2_cash_distribution_processq2_3_how_mach_cash}
+              <br />
+              25th: ${data['25_s2_cash_distribution_processq2_3_how_mach_cash']}
+              <br />
+              75th: ${data['75_s2_cash_distribution_processq2_3_how_mach_cash']}
+            </div>
+          `}
+        >
+          <BulletGraph
+            title=""
+            textLabel=""
+            scaleMin={data.min_s2_cash_distribution_processq2_3_how_mach_cash}
+            scaleMax={data.max_s2_cash_distribution_processq2_3_how_mach_cash}
+            symbolMarker={
+              data['25_s2_cash_distribution_processq2_3_how_mach_cash']
+            }
+            performanceVal={
+              data['75_s2_cash_distribution_processq2_3_how_mach_cash']
+            }
+            badVal={0}
+            satisfactoryVal={
+              data.avg_s2_cash_distribution_processq2_3_how_mach_cash
+            }
+            height={40}
+            width={600}
+            badColor="#ffffff"
+            satisfactoryColor="#ee4e4e"
+            goodColor="#ff776f"
+            isActiveColor
+            opacity={0.6}
+          />
+        </div>
+        <ReactTooltip
+          className="graph__tooltip"
+          id="averageCashReceived"
+          html
+        />
+      </div>
+    );
+  }
+}
 
 export default Chart;
